4ad5527e2422783231098d1471537a5bf8d459be
kit-kiten <malysnikitaenko@gmail.com>
2022-08-20 18:55:57 +1000

    layout, scss, created first components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import './App.css';
+import './scss/app.scss'
+
+import Header from "./components/Header";
+import Categories from "./components/Components";
+import Sort from "./components/Sort";
+import PizzaBlock from "./components/PizzaBlock";
 
 function App() {
   return (
-    <div className="App">
-      Hello world
-    </div>
+      <div className="wrapper">
+          <Header />
+          <div className="content">
+              <div className="container">
+                  <div className="content__top">
+                      <Categories />
+                      <Sort />
+                  </div>
+                  <h2 className="content__title">Все пиццы</h2>
+                  <div className="content__items">
+                      <PizzaBlock title={'Чизбургер-пицца'} price={395} />
+                      <PizzaBlock title={'Сырная'} price={450} />
+                  </div>
+              </div>
+          </div>
+      </div>
   );
 }
 
