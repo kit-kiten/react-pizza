9839cb4bd484e56f49f24e6c492e389673106db3
kit-kiten <malysnikitaenko@gmail.com>
2022-08-21 21:18:31 +1000

    added backend(mockapi) with using axios, render through hook useEffect

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
 import './scss/app.scss'
 
+import React from "react";
+import axios from "axios";
+
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
 
-import pizzas from "./assets/pizzas.json";
-
 function App() {
-  return (
-      <div className="wrapper">
-          <Header />
-          <div className="content">
-              <div className="container">
-                  <div className="content__top">
+    const [items, setItems] = React.useState([]);
+
+    React.useEffect(() => {
+        axios.get('https://6301e0a3c6dda4f287ae88c3.mockapi.io/items').then(response => {
+            setItems(response.data);
+        })
+    }, [])
+
+    return (
+        <div className="wrapper">
+            <Header />
+            <div className="content">
+                <div className="container">
+                    <div className="content__top">
                       <Categories />
                       <Sort />
-                  </div>
-                  <h2 className="content__title">Все пиццы</h2>
-                  <div className="content__items">
-                      {
-                          pizzas.map(obj => (
+                    </div>
+                    <h2 className="content__title">Все пиццы</h2>
+                    <div className="content__items">
+                        {
+                          items.map(obj => (
                               <PizzaBlock
                                   key={obj.id}
                                   {...obj}
                               />
                           ))
-                      }
+                        }
                   </div>
               </div>
           </div>
